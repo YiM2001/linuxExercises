7355ac27d18477311b196f440bfde55f6a7fc7fc
YiM2001 <yma329@wisc.edu>
2023-10-13 10:46:32 -0500

    Get an answer of 8444

--- digits.sh
+++ digits.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+pattern="[0-1]{4}"
+total=0
+
+#for i in $(seq 1000 2000);do
+#    if [[ $i =~ $pattern ]]; then
+#	total=$(($total+$i));
+#    fi
+#done
+
+init=1000
+while [[ $init -lt 2000 ]];
+do
+    if [[ $init =~ $pattern ]]; then
+	total=$(($total+$init));
+    fi
+    init=$(($init+1))
+done
+
+echo $total
+
+
+
+
+
+
+
