1549abfc21071fa247d643b4a1e9172dec9ea03b
YiM2001 <yma329@wisc.edu>
2023-10-19 14:00:31 -0500

    Finished by Chenghao

--- mean.sh
+++ mean.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+n=$1
+file=$2
+
+
+
+cat $file | tail +2 | cut -d ',' -f $n | { sum=$(echo "scale=6; 0.00000" | bc); count=$(echo "scale=6; 0.00000" | bc);  while read num
+do
+    if [[ ($num -ne 0) ]]; then
+	sum=$(echo "scale=6; $sum+$num" | bc)
+	count=$(($count+1))
+    fi
+done
+echo $sum
+echo $count
+mean=$(echo "scale=6; $sum/$count" | bc)
+echo $mean
+}
